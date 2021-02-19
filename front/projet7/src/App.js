70d4c2e7da6ae9106b4e765adac3109be0655a98
hugo polchetti <h.polchetti@gmail.com>
2021-02-19 18:22:01 +0100

    first commit

---
+++ front/projet7/src/App.js
@@ -0,0 +1,12 @@
+import PageAcceuil from "./components/pageAcceuil/pageAcceuil";
+
+function App() {
+	return (
+		<div className="container">
+			<PageAcceuil />
+			{/* il faudra ajouter les components sur site ici  */}
+		</div>
+	);
+}
+
+export default App;
---
+++ front/projet7/src/components/pageAcceuil/pageAcceuil.js
@@ -0,0 +1,25 @@
+const PageAcceuil = () => {
+	return (
+		<>
+			<div>
+				<div>
+					<div className="blockLogo">
+						<div>
+							<img
+								src="images/icon-left-font-monochrome-black.png"
+								alt="logo de Groupomania"
+								className="logoPageAcceuil"
+							/>
+							<h2>
+								Avec Groupomania, partagez et restez en contact avec vos collègues
+							</h2>
+						</div>
+					</div>
+					<div className="blockForm"></div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default PageAcceuil;
