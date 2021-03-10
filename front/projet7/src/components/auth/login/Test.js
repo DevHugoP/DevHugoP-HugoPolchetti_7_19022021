4ceae3c8c261071f751f22d67f34c28c9c4e3e64
hugo polchetti <h.polchetti@gmail.com>
2021-03-10 18:53:17 +0100

    micro changes

--- front/projet7/src/components/auth/login/Test.js
+++ front/projet7/src/components/auth/login/Test.js
@@ -14,13 +14,13 @@ export default class Test extends React.Component {
 			}
 			return <h2>swag</h2>;
 		});
-		// axios
-		// 	.put(`http://localhost:5000/api/messages/7e9d6398-1276-4f71-b376-265f2cc9777d`, {
-		// 		content: "on test la methode put avec Axios depuis le front",
-		// 		likes: 2
-		// 	})
-		// 	.then((res) => {
-		// 		console.log(res.data);
-		// 	});
+		axios
+			.put(`http://localhost:5000/api/messages/7e9d6398-1276-4f71-b376-265f2cc9777d`, {
+				content: "on test la methode put avec Axios depuis le front",
+				likes: 2
+			})
+			.then((res) => {
+				console.log(res.data);
+			});
 	}
 }
